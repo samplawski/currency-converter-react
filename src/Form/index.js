0c22329f06ba82940bd4f112cbd622b624ea9723
samplawski <tsamplawski@gmail.com>
2025-11-24 18:29:35 +0100

    feat: Input component created

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,48 +1,14 @@
 import "./style.css";
 import Buttons from "../Buttons/inbex.js";
 import Output from "../Output/index.js";
+import Input from "../Input/index.js";
 
 const Form = () => (
-<form className="form js-form">
-          <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator walut</legend>
-
-            <p>
-              <label>
-                <span className="form__labelText">Wybierz walutę:</span>
-                <select
-                  name="currency"
-                  className="form__currencyField js-currency"
-                >
-                  <option value="USD">&#x24; USD</option>
-                  <option value="EUR">&#8364; EUR</option>
-                  <option value="GBP">&#163; GBP</option>
-                </select>
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <span className="form__labelText">
-                  Ile PLN chcesz wymienić:
-                </span>
-                <input
-                  name="PLN"
-                  className="form__currencyField js-exchangedPLN"
-                  type="number"
-                  required
-                  min="0"
-                  step="0.01"
-                  placeholder="Ile złotych wymieniasz?"
-                />
-              </label>
-            </p>
-          </fieldset>
-          
-          <Buttons/>
-          
-       <Output/>
-        </form>
+  <form className="form js-form">
+    <Input />
+    <Buttons />
+    <Output />
+  </form>
 );
 
 export default Form;
