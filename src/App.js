37e870aafd5a64ae524929ef6bffe671b1f0e923
samplawski <tsamplawski@gmail.com>
2025-11-24 15:26:04 +0100

    chore: wrong favicon reference removed from imanifes.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TableContainer from "./TableContainer";
 import Table from "./Table";
 import Body from "./Body";
 import Converter from "./Converter";
+import Form from "./Form";
 
 
 function App() {
@@ -13,70 +14,7 @@ function App() {
       </TableContainer>
 
       <Converter className="converter">
-         <form className="form js-form">
-          <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator walut</legend>
-
-            <p>
-              <label>
-                <span className="form__labelText">Wybierz walutę:</span>
-                <select
-                  name="currency"
-                  className="form__currencyField js-currency"
-                >
-                  <option value="USD">&#x24; USD</option>
-                  <option value="EUR">&#8364; EUR</option>
-                  <option value="GBP">&#163; GBP</option>
-                </select>
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <span className="form__labelText">
-                  Ile PLN chcesz wymienić:
-                </span>
-                <input
-                  name="PLN"
-                  className="form__currencyField js-exchangedPLN"
-                  type="number"
-                  required
-                  min="0"
-                  step="0.01"
-                  placeholder="Ile złotych wymieniasz?"
-                />
-              </label>
-            </p>
-          </fieldset>
-          <p>
-            <button
-              className="form__button"
-              type="submit"
-              value="Przelicz"
-              title="Przelicz"
-            >
-              Przelicz
-            </button>
-          </p>
-          <p>
-            <button
-              className="form__button"
-              type="reset"
-              value="Wyczyść"
-              title="Wyczyść"
-            >
-              Wyczyść
-            </button>
-          </p>
-          <p>
-            <label>
-              Po przewalutowaniu dostaniesz:
-              <strong className="js-output">
-                Wybierz walutę i wpisz kwotę do wymiany.
-              </strong>
-            </label>
-          </p>
-        </form>
+         <Form/>
       </Converter>
 
       <footer className="footer">
