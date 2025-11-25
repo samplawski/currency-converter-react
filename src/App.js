fdd9f499a641e3bfb0a56133dfd2819f03e1c2d8
samplawski <tsamplawski@gmail.com>
2025-11-25 19:35:57 +0100

    feat: useState hook used to display currencies' data in the currencies' table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Body from "./Body";
 import TableContainer from "./TableContainer";
 import Table from "./Table";
@@ -7,14 +7,49 @@ import Form from "./Form";
 import Footer from "./Footer";
 
 function App() {
+  const [currencies, setCurrencies] = useState([
+    {
+      id: 1,
+      name: "United States dollar",
+      namePL: "Dolar amerykański",
+      code: "USD",
+      symbol: "$",
+      rate: 3.6491,
+    },
+    {
+      id: 2,
+      name: "Euro",
+      namePL: "Euro",
+      code: "EUR",
+      symbol: "€",
+      rate: 4.2274,
+    },
+    {
+      id: 3,
+      name: "Pound sterling",
+      namePL: "Brytyjski funt szterling",
+      code: "GBP",
+      symbol: "£",
+      rate: 4.8189,
+    },
+    {
+      id: 4,
+      name: "Bitcoin",
+      namePL: "Bitcoin",
+      code: "BTC",
+      symbol: "₿",
+      rate: 320727.6,
+    },
+  ]);
+
   return (
     <Body>
       <TableContainer>
-        <Table />
+        <Table currencies={currencies} />
       </TableContainer>
 
       <Converter>
-        <Form />
+        <Form currencies={currencies} />
       </Converter>
 
       <Footer />
--- src/Table/index.js
+++ src/Table/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Table = () => (
+const Table = ({ currencies }) => (
   <table className="table">
     <caption className="table__caption">Waluty w kalkulatorze.</caption>
     <thead>
@@ -18,41 +18,24 @@ const Table = () => (
           Symbol
         </th>
         <th className="table__header" scope="col">
-          Aktualny kurs {/* aktualny kurs walut wpisywać w script.js:12:14, 13:14, 14:15) */}
+          Aktualny kurs
+          {/* aktualny kurs walut wpisywać App.js w liniach: 17, 25, 33) */}
         </th>
       </tr>
     </thead>
 
     <tbody>
-      <tr className="table__row">
-        <th className="table__header" scope="row">
-          United States dollar
-        </th>
-        <td className="table__cell">Dolar amerykański</td>
-        <td className="table__cell">USD</td>
-        <td className="table__cell">&#x24;</td>
-        <td className="table__cell js-USD"></td>
-      </tr>
-
-      <tr className="table__row">
-        <th className="table__header" scope="row">
-          Euro
-        </th>
-        <td className="table__cell">Euro</td>
-        <td className="table__cell">EUR</td>
-        <td className="table__cell">&#8364;</td>
-        <td className="table__cell js-EUR"></td>
-      </tr>
-
-      <tr className="table__row">
-        <th className="table__header" scope="row">
-          Pound sterling
-        </th>
-        <td className="table__cell">Brytyjski funt szterling</td>
-        <td className="table__cell">GBP</td>
-        <td className="table__cell">&#163;</td>
-        <td className="table__cell js-GBP"></td>
-      </tr>
+      {currencies.map((currency) => (
+        <tr key={currency.id} className="table__row">
+          <th className="table__header" scope="row">
+            {currency.name}
+          </th>
+          <td className="table__cell">{currency.namePL}</td>
+          <td className="table__cell">{currency.code}</td>
+          <td className="table__cell">{currency.symbol}</td>
+          <td className="table__cell">{currency.rate.toFixed(2)} zł</td>
+        </tr>
+      ))}
     </tbody>
   </table>
 );
