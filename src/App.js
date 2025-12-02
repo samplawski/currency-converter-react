49d324f5ca10d57e8d457717cb01c608a380f5d7
samplawski <tsamplawski@gmail.com>
2025-12-02 18:04:26 +0100

    refactor: currencies array moved to a separate file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React, { useState } from "react";
+import {currencies} from "./currencies";
 import Body from "./Body";
 import Table from "./Table";
 import Converter from "./Converter";
 import Footer from "./Footer";
 
 function App() {
-  const [currencies, setCurrencies] = useState([
-    {
-      id: 1,
-      name: "United States dollar",
-      namePL: "Dolar amerykański",
-      code: "USD",
-      symbol: "$",
-      rate: 3.6491,
-    },
-    {
-      id: 2,
-      name: "Euro",
-      namePL: "Euro",
-      code: "EUR",
-      symbol: "€",
-      rate: 4.2274,
-    },
-    {
-      id: 3,
-      name: "Pound sterling",
-      namePL: "Brytyjski funt szterling",
-      code: "GBP",
-      symbol: "£",
-      rate: 4.8189,
-    },
-    {
-      id: 4,
-      name: "Bitcoin",
-      namePL: "Bitcoin",
-      code: "BTC",
-      symbol: "₿",
-      rate: 320727.6,
-    },
-  ]);
+
 
   const [amountPLN, setAmountPLN] = useState("");
 
---
+++ src/currencies.js
@@ -0,0 +1,34 @@
+export const currencies = [
+    {
+      id: 1,
+      name: "United States dollar",
+      namePL: "Dolar amerykański",
+      code: "USD",
+      symbol: "$",
+      rate: 3.6491,
+    },
+    {
+      id: 2,
+      name: "Euro",
+      namePL: "Euro",
+      code: "EUR",
+      symbol: "€",
+      rate: 4.2274,
+    },
+    {
+      id: 3,
+      name: "Pound sterling",
+      namePL: "Brytyjski funt szterling",
+      code: "GBP",
+      symbol: "£",
+      rate: 4.8189,
+    },
+    {
+      id: 4,
+      name: "Bitcoin",
+      namePL: "Bitcoin",
+      code: "BTC",
+      symbol: "₿",
+      rate: 320727.6,
+    },
+  ];
